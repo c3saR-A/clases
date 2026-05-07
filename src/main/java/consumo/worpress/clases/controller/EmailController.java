1f179eaf5af1019610aa7c3bc82bfb50eec776f9
César <06cesar.leon@gmail.com>
2026-05-06 23:45:09 -0600

    manejor de varibles de entorno

--- src/main/java/consumo/worpress/clases/controller/EmailController.java
+++ src/main/java/consumo/worpress/clases/controller/EmailController.java
@@ -23,15 +23,14 @@ public class EmailController {
         emailService.sendSimpleEmail(
                 "06cesar.leon@gmail.com",
                 "Palabras Sagradas NIGGA!",
-                "Wassup, can a loc come up in your crib?" +
-                        "Man, fuck you. I'll see you at work." +
-                        "Ah, nigga, don't hate me cause I'm beautiful, nigga." +
+                "Wassup, can a loc come up in your crib? \n" +
+                        "Man, fuck you. I'll see you at work. \n" +
+                        "Ah, nigga, don't hate me cause I'm beautiful, nigga. \n" +
                         "Maybe if you got rid of that old yee-yee-ass haircut you got," +
-                        "you'd get some bitches on your dick." +
+                        "you'd get some bitches on your dick. \n" +
                         "Oh, better yet, maybe Tanisha'll call your dog-ass" +
                         "if she ever stop fucking with that brain surgeon or lawyer she fucking with." +
-                        "Nigga..." +
-                        "\n" +
+                        "Nigga... \n" +
                         "What?!");
         return "Email Enviado Crack";
     }
